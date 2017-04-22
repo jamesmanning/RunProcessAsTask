fffc7fdc329cae6aff872092866f78d36d2f8ae0
Eamon Nerbonne <eamon@nerbonne.org>
2017-04-22 23:00:31 +0200

    Autoformat and use expression-bodied members

--- src/RunProcessAsTask/ProcessEx.overloads.cs
+++ src/RunProcessAsTask/ProcessEx.overloads.cs
@@ -2,24 +2,18 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Security;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RunProcessAsTask
 {
-        // these overloads match the ones in Process.Start to make it a simpler transition for callers
-        // see http://msdn.microsoft.com/en-us/library/system.diagnostics.process.start.aspx
-        public partial class ProcessEx
-        {
-                public static Task<ProcessResults> RunAsync(string fileName)
-                {
-                        return RunAsync(new ProcessStartInfo(fileName));
-                }
+    // these overloads match the ones in Process.Start to make it a simpler transition for callers
+    // see http://msdn.microsoft.com/en-us/library/system.diagnostics.process.start.aspx
+    public partial class ProcessEx
+    {
+        public static Task<ProcessResults> RunAsync(string fileName)
+            => RunAsync(new ProcessStartInfo(fileName));
 
-                public static Task<ProcessResults> RunAsync(string fileName, string arguments)
-                {
-                        return RunAsync(new ProcessStartInfo(fileName, arguments));
-                }
-        }
+        public static Task<ProcessResults> RunAsync(string fileName, string arguments)
+            => RunAsync(new ProcessStartInfo(fileName, arguments));
+    }
 }

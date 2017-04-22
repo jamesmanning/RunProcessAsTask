959b5ea05bfcb042a15b28f68d98e5fcc8dbe175
Eamon Nerbonne <eamon@nerbonne.org>
2017-04-22 22:57:41 +0200

    syntactic cleanup ProcessResults & seal

--- src/RunProcessAsTask/ProcessResults.cs
+++ src/RunProcessAsTask/ProcessResults.cs
@@ -2,55 +2,25 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 
 namespace RunProcessAsTask
 {
-        public class ProcessResults : IDisposable
+    public sealed class ProcessResults : IDisposable
+    {
+        public ProcessResults(Process process, string[] standardOutput, string[] standardError)
         {
-                private readonly Process _process;
-                private readonly string[] _standardOutput;
-                private readonly string[] _standardError;
-                private readonly int _exitCode;
-                private readonly TimeSpan _runTime;
-
-                public ProcessResults(Process process, string[] standardOutput, string[] standardError)
-                {
-                        _process = process;
-                        _exitCode = process.ExitCode;
-                        _runTime = process.ExitTime - process.StartTime;
-                        _standardOutput = standardOutput;
-                        _standardError = standardError;
-                }
-
-                public Process Process
-                {
-                        get { return _process; }
-                }
-
-                public int ExitCode
-                {
-                        get { return _exitCode; }
-                }
-
-                public TimeSpan RunTime
-                {
-                        get { return _runTime; }
-                }
-
-                public string[] StandardOutput
-                {
-                        get { return _standardOutput; }
-                }
-
-                public string[] StandardError
-                {
-                        get { return _standardError; }
-                }
-
-                public void Dispose()
-                {
-                        _process.Dispose();
-                }
+            Process = process;
+            ExitCode = process.ExitCode;
+            RunTime = process.ExitTime - process.StartTime;
+            StandardOutput = standardOutput;
+            StandardError = standardError;
         }
+
+        public Process Process { get; }
+        public int ExitCode { get; }
+        public TimeSpan RunTime { get; }
+        public string[] StandardOutput { get; }
+        public string[] StandardError { get; }
+        public void Dispose() { Process.Dispose(); }
+    }
 }
